0e1a4718b6b525cf361c76e955d92007c695a9f4
Fernando Carril <facarril@gmail.com>
2018-05-06 19:49:48 -0300

    Added CSS to the product component

--- src/product/components/Product.js
+++ src/product/components/Product.js
@@ -22,36 +22,35 @@ class Product extends Component {
     return (
       <div className="product-container">
         <div className="product">
-          <div className="product-information">
-            <div className="product-image">
-              <img src={product.picture} />
+          <section className="product-information">
+            <div class="product-image-container">
+              <img className="product-image" src={product.picture} />
             </div>
 
             <div className="product-description">
-              <div className="product-description-title">Descripción</div>
-              <div className="product-description-text">
-                <pre>{product.description}</pre>
-              </div>
+              <h2 className="product-description-title">Descripción</h2>
+              <p className="product-description-text">{product.description}</p>
             </div>
-          </div>
+          </section>
 
-          <div className="product-checkout">
+          <section className="product-checkout">
             <div className="product-status">
               <span>{this.formatMessage(product.condition)}</span>
               <span> - </span>
               <span>{this.formatNumber(product.sold_quantity)}</span>
               <span> vendidos</span>
             </div>
-            <div className="product-title">{product.title}</div>
+            <h1 className="product-title">{product.title}</h1>
             <div className="product-price">
               <span className="product-price-symbol">{product.price.currency}</span>
               <span className="product-price-fraction">{this.formatNumber(product.price.amount)}</span>
               <span className="product-price-decimals">{product.price.decimals}</span>
             </div>
-            <div></div>
-          </div>
+            <div>
+              <button type="button" className="checkout-button">Comprar</button>
+            </div>
+          </section>
         </div>
-      <div>{this.props.product.title}</div>
       </div>
     );
   }
